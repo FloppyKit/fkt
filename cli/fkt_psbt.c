ace332bef463ec65089c75ca3f57f366f23d79c0
FloppyKit <floppykit@gitfkt.dev>
2026-06-24 23:16:23 -0500

    Add P2WSH multisig and P2SH-P2WPKH partial-sig signing for Sparrow

---
+++ cli/fkt_psbt.c
@@ -0,0 +1,9 @@
+#include "fkt_psbt.h"
+#include "fkt_sha256.h"
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* placeholder - will load from file
